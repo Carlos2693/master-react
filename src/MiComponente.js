0cda8f5c464263e21c22da92fe97ecb6de1b3207
Carlos Buruel <charly.ob.26@gmail.com>
2022-10-15 06:50:00 -0500

    Add first component

---
+++ src/MiComponente.js
@@ -0,0 +1,13 @@
+// importar modulos de react
+import React from "react";
+
+// Función
+const MiComponente = () => {
+
+    return (
+        <p>Este es mi primer componente</p>
+    )
+}
+
+// Export
+export default MiComponente;
