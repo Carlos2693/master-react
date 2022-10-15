984092aac91dd06cbdb9002fa4559a5ca08a21e4
Carlos Buruel <charly.ob.26@gmail.com>
2022-10-15 09:30:05 -0500

    Show component's data in view

--- src/MiComponente.js
+++ src/MiComponente.js
@@ -4,10 +4,31 @@ import React from "react";
 // Función
 const MiComponente = () => {
 
+    let nombre = "Carlos"
+    let web = "carlos.buruel.ortiz.com"
+
+    let usuario = {
+        nombre: "Carlos",
+        apellido: "Buruel",
+        web: "carlos.buruel.ortiz.com"
+    }
+
     return (
         <>
             <hr />
             <h2>Componente creado</h2>
+            <h3>Datos del usuario: </h3>
+            <ul>
+                <li>
+                    Nombre: <strong>{usuario.nombre}</strong>
+                </li>
+                <li>
+                    Apellido: <strong>{usuario.apellido}</strong>
+                </li>
+                <li>
+                    Web: <strong>{usuario.web}</strong>
+                </li>
+            </ul>
             <p>Este es mi primer componente</p>
             <ul>
                 <li>
