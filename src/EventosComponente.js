dc337a7cd577397daa902c575968b254db51aea9
Carlos Buruel Ortiz <charly.ob.26@gmail.com>
2022-10-21 08:17:52 -0500

    Double Click event on React

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -6,6 +6,10 @@ export const EventosComponente = () => {
         alert("Has dado click al botón!! " + nombre)
     }
 
+    function hasDadoDobleClick(e) {
+       alert('Has dado doble click!!!')
+    }
+
   return (
     <div>
         <h1>Eventos en React</h1>
@@ -17,9 +21,14 @@ export const EventosComponente = () => {
             console.log("I'm click event")
         }}>Push it!</button>
         */}
-        <button onClick={ e => hasDadoClick(e, "Carlos") }>Dame click!</button>
+        <p>
+          <button onClick={ e => hasDadoClick(e, "Carlos") }>Dame click!</button>
+        </p>
 
-
+        <p>
+        <button onDoubleClick={ hasDadoDobleClick }>Dame click!</button>  
+        </p>
+        
     </div>
   )
 }
