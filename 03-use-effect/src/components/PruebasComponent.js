fbdf32a91c9507c495931e7e514f2fb971761cad
Carlos Buruel <charly.ob.26@gmail.com>
2022-10-31 08:24:39 -0600

    Practice with useEffect

---
+++ 03-use-effect/src/components/PruebasComponent.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+
+export const PruebasComponent = () => {
+
+    const [user, setUser] = useState('Carlos Buruel')
+    const [date, setDate] = useState('17-06-1993')
+    const [counter, setCounter] = useState(0)
+
+    const changeUser = e => {
+        setUser(e.target.value)
+    }
+
+    const changeDate = e => {
+        setDate(Date.now())
+    }
+
+    // Execute only one, when component starts
+    useEffect(() => {
+        console.log('Has cargado el componente');
+    }, [])
+
+    // Execute once name changing
+    useEffect(() => {
+
+        setCounter(counter + 1)
+        console.log('Has modificado el usuario: ' + counter);
+
+    }, [date, user])
+
+    return (
+        <div>
+            <h1>El efecto - Hook useEffect</h1>
+
+            <strong>{user}</strong>
+            <strong className={ counter >= 10 ? 'label label-green' : 'label' }>{date}</strong>
+
+            <p>
+                <input
+                    type='text'
+                    onChange={changeUser}
+                    placeholder='Cambiar el nombre' />
+            </p>
+
+            <button onClick={changeDate}>Cambiar fecha</button>
+
+        </div>
+    )
+}
