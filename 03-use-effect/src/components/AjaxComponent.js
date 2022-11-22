5b536abc56e6fc88d6f476c52bafddaa99bf206b
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2022-11-21 19:21:37 -0600

    AJAX & useEffect

---
+++ 03-use-effect/src/components/AjaxComponent.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+
+export const AjaxComponent = () => {
+
+  const [users, setUsers] = useState([])
+
+// Generic / basic
+const getStaticUsers = () => {
+    setUsers([
+        {
+            "id": 1,
+            "email": "carlos.buruel@reqres.in",
+            "first_name": "Carlos",
+            "last_name": "Buruel",
+            "avatar": "https://reqres.in/img/faces/7-image.jpg"
+          },
+          {
+            "id": 2,
+            "email": "john.bonachon@reqres.in",
+            "first_name": "John",
+            "last_name": "Bonachon",
+            "avatar": "https://reqres.in/img/faces/8-image.jpg"
+          },
+          {
+            "id": 3,
+            "email": "sheldon.cooper@reqres.in",
+            "first_name": "Sheldon",
+            "last_name": "Cooper",
+            "avatar": "https://reqres.in/img/faces/9-image.jpg"
+          },
+    ])
+}
+
+useEffect(() => {
+    getStaticUsers()
+}, [])
+
+  return (
+    <div>
+        <h2>Listado de usuarios via Ajax</h2>
+        <ol className='usuarios'>
+            {
+                users.map( user => {
+                    return <li key={user.id}>{user.first_name} {user.last_name}</li>
+                })
+            }
+        </ol>
+
+
+    </div>
+  )
+}
