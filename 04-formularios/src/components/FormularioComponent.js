2ee8ce3d6251dc61a87d70f251dea83bd9b91ba7
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2022-11-25 20:07:09 -0600

    More forms more events

--- 04-formularios/src/components/FormularioComponent.js
+++ 04-formularios/src/components/FormularioComponent.js
@@ -12,17 +12,32 @@ export const FormularioComponent = () => {
             name: data.name.value,
             last_name: data.last_name.value,
             gender: data.gender.value,
-            bio: data.bio.value
+            bio: data.bio.value,
+            send: data.send.value
         }
 
         setUser(userData)
+    }
+
+    const changeData = e => {
+        let inputName = e.target.name
+        let editUser = user
+
+        // editUser[inputName] = e.target.value
+
+        setUser(
+            previousState => ({
+                ...previousState,
+                [inputName]: e.target.value
+            })
+        )
     }
 
   return (
     <div>
         <h1>Formularios con React</h1>
 
-        { user.bio && user.bio.length >= 1 &&
+        { user.send  &&
             (
                 <div className='info_usuario label label-gray'>
                     {user.name} {user.last_name} es un {user.gender}
@@ -32,14 +47,25 @@ export const FormularioComponent = () => {
         }
 
         <form onSubmit={getFormData}>
-            <input type="text" placeholder='Nombre' name='name'/>
-            <input type="text" placeholder='Apellido' name='last_name'/>
-            <select name='gender'>
+            <input
+                type="text"
+                placeholder='Nombre'
+                name='name'
+                onChange={changeData}/>
+            <input
+                type="text"
+                placeholder='Apellido'
+                name='last_name'
+                onChange={changeData}/>
+            <select name='gender' onChange={changeData}>
                 <option value="hombre">Hombre</option>
                 <option value="mujer">Mujer</option>
             </select>
-            <textarea placeholder='Biografia' name='bio'></textarea>
-            <input type="submit" value="Enviar" />
+            <textarea
+                placeholder='Biografia'
+                name='bio'
+                onChange={changeData} ></textarea>
+            <input type="submit" value="Enviar" name="send" />
         </form>
     </div>
   )
