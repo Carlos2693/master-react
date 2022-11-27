8af092562126b17b7488e90af96a28de15d9dd15
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2022-11-27 07:38:33 -0600

    Form for create movies

--- 06-proyecto1/src/components/Crear.js
+++ 06-proyecto1/src/components/Crear.js
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Crear = () => {
+
+  const componentTitle = "Añadir pelicula"
+  const [movie, setMovie] = useState({
+    title: '',
+    description: ''
+  })
+  const {title, description} = movie
+
+  const getFormData = e => {
+    e.preventDefault()
+    
+    // get form data
+    let target = e.target
+    let _title = target.title.value
+    let _description = target.description.value
+
+    // create movie object
+    let _movie = {
+      id: new Date().getTime(),
+      title: _title,
+      description: _description
+    }
+
+    setMovie(_movie)
+  }
+
   return (
     <div className="add">
-        <h3 className="title">Añadir pelicula</h3>
-        <form>
-            <input type="text" placeholder="Titulo"/>
-            <textarea placeholder="Descripcion"></textarea>
-            <input type="submit" value="Guardar" />
+        <h3 className="title">{componentTitle}</h3>
+
+        <strong>
+          {(title && description) && "Has creado la pelicula: " + title}
+        </strong>
+        
+        <form onSubmit={getFormData}>
+            <input
+              id='title'
+              name='title'
+              type="text"
+              placeholder="Titulo"/>
+            <textarea
+              id='description'
+              name='description'
+              placeholder="Descripcion"></textarea>
+            <input
+              id='save'
+              type="submit"
+              value="Guardar" />
         </form>
     </div>
   )
