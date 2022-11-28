18a030b060b792864a60400a1c4fd222aa62f10e
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2022-11-28 08:21:05 -0600

    Get list with useEffect

--- 06-proyecto1/src/components/Listado.js
+++ 06-proyecto1/src/components/Listado.js
@@ -1,39 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Listado = () => {
+
+  const [listState, setListState] = useState([])
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
+  const getMovies = () => {
+    let movies = JSON.parse(localStorage.getItem('movies'))
+
+    setListState(movies)
+  }
+  
   return (
     <>
-        <article className="movie-item">
-                <h3 className="title">Desarrollo mobile</h3>
-                <p className="description">carlosburuel.com.mx</p>
+      {listState != null ?
+        listState.map(movie => {
+          return (
+            <article key={movie.id} className="movie-item">
+                <h3 className="title">{movie.title}</h3>
+                <p className="description">{movie.description}</p>
 
                 <button className="edit">Editar</button>
                 <button className="delete">Borrar</button>
             </article>
-
-            <article className="movie-item">
-                <h3 className="title">Desarrollo mobile</h3>
-                <p className="description">carlosburuel.com.mx</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="movie-item">
-                <h3 className="title">Desarrollo mobile</h3>
-                <p className="description">carlosburuel.com.mx</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="movie-item">
-                <h3 className="title">Desarrollo mobile</h3>
-                <p className="description">carlosburuel.com.mx</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+          )
+        })
+        : <h2>No hay peliculas para mostrar</h2>
+      }
     </>
   )
 }
