8b95f32652af6db947dc2b260d87e8c02db6b4b1
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2022-12-05 07:18:17 -0600

    Nav menu

--- 07-routes/src/routers/RouterPrincipal.js
+++ 07-routes/src/routers/RouterPrincipal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, NavLink, BrowserRouter } from 'react-router-dom'
 import { Articulos } from '../components/Articulos'
 import { Contacto } from '../components/Contacto'
 import { Error } from '../components/Error'
@@ -7,16 +7,34 @@ import { Inicio } from '../components/Inicio'
 
 export const RouterPrincipal = () => {
   return (
-    <div>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Inicio />} />
-                <Route path='/inicio' element={<Inicio />} />
-                <Route path='/articulos' element={<Articulos />} />
-                <Route path='/contacto' element={<Contacto />} />
-                <Route path='*' element={<Error />} />
-            </Routes>
-        </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <h1>Cabecera</h1>
+      <hr />
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/inicio'>Inicio</NavLink>
+          </li>
+          <li>
+            <NavLink to='/articulos'>Articulos</NavLink>
+          </li>
+          <li>
+            <NavLink to='/contacto'>Contacto</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+
+      <section className='contenido-principal'>
+        <Routes>
+          <Route path='/' element={<Inicio />} />
+          <Route path='/inicio' element={<Inicio />} />
+          <Route path='/articulos' element={<Articulos />} />
+          <Route path='/contacto' element={<Contacto />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </section>
+    </BrowserRouter>
   )
 }
