25d84fa111d4766833dd65fdb2ee4831685d78ee
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2022-12-09 07:54:44 -0600

    Hook useNavigate

--- 07-routes/src/components/Persona.js
+++ 07-routes/src/components/Persona.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export const Persona = () => {
 
     let {name = "Carlos", last_name = "Buruel"} = useParams()
+    const toNavigate = useNavigate()
+
+    const send = (e) => {
+      e.preventDefault()
+      let name = e.target.name.value
+      let lastName = e.target.lastName.value
+      let url = `/persona/${name}/${lastName}`
+
+      if (name.length <= 0 && lastName.length <= 0) {
+        toNavigate('/inicio')
+      } else if (name === 'contacto') {
+        toNavigate('/contacto')
+      } else {
+        toNavigate(url)
+      }
+    }
 
   return (
     <div>
         <h1>Pagina de Persona: {name} {last_name}</h1>
         <p>Está es la página de persona</p>
+
+        <form onSubmit={send}>
+          <input type='text' name='name' />
+          <input type='text' name='lastName' />
+          <input type='submit' name='send' value='Enviar' />
+        </form>
     </div>
   )
 }
